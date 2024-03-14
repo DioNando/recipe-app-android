286ee712ad5c7150ab976a0f0bfa97d9e5f1eacf
David Fernando <mahefa.fernando@gmail.com>
2024-03-14 20:18:20 +0000

    Feat(res): add welcome, login, register pages

---
+++ app/src/main/java/com/example/recipeapp/WelcomeActivity.java
@@ -0,0 +1,36 @@
+package com.example.recipeapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_welcome);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void login(View view) {
+        startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+    }
+
+    public void mainActivity(View view) {
+        startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+    }
+}
