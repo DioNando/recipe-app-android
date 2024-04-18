53f2df87c757f720b3ad01c06b7d31b51fc7afbb
Ahmed Amine El Aboudi <122729389+Devlover1@users.noreply.github.com>
2024-04-18 16:24:31 +0200

    feature: show random image on homepage

--- app/src/main/java/com/example/recipeapp/MainActivity.java
+++ app/src/main/java/com/example/recipeapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.recipeapp;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Log;
@@ -11,6 +12,9 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.recipeapp.Adapters.RandomRecipeAdapter;
+import com.example.recipeapp.Listeners.RandomRecipeResponseListener;
+import com.example.recipeapp.Models.RandomRecipeApiResponse;
 import com.example.recipeapp.data.Recipe;
 import com.example.recipeapp.databinding.ActivityMainBinding;
 
@@ -23,73 +27,41 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
-    private RecipeAdapter recipeAdapter;
+     ProgressDialog dialog;
+     RequestManager manager;
 
-    private SpoonacularApi spoonacularApi;
-
+     RandomRecipeAdapter randomRecipeAdapter;
+     RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Action de votre FloatingActionButton
-            }
-        });
-
-
-        // Initialiser le RecyclerView
-        RecyclerView recyclerView = binding.recyclerView;
-
-
-        // Spécifier le nombre de colonnes pour le GridLayoutManager
-        int numberOfColumns = 2; // Vous pouvez ajuster ce nombre selon vos préférences
-
-        // Créer un GridLayoutManager avec le nombre de colonnes spécifié
-        GridLayoutManager layoutManager = new GridLayoutManager(MainActivity.this, numberOfColumns, RecyclerView.VERTICAL, false);
-
-        // Associer le GridLayoutManager à votre RecyclerView
-        recyclerView.setLayoutManager(layoutManager);
-
-
-        recipeAdapter = new RecipeAdapter(new ArrayList<>());
-        recyclerView.setAdapter(recipeAdapter);
-
-        spoonacularApi = RetrofitClient.getClient().create(SpoonacularApi.class);
-
-        // Effectuez la recherche de recettes
-        searchRecipes("3b603fcae7834594ae55b4052a02428d", "pasta");
+        setContentView(R.layout.activity_main);
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("Loading...");
+        manager = new RequestManager(this);
+        manager.getRandomRecipies(randomRecipeResponseListener);
+        dialog.show();
     }
 
-    private void searchRecipes(String apiKey, String query) {
-        Call<RecipeSearchResponse> call = spoonacularApi.searchRecipes(apiKey, query);
-        call.enqueue(new Callback<RecipeSearchResponse>() {
+        private final RandomRecipeResponseListener randomRecipeResponseListener=new RandomRecipeResponseListener() {
             @Override
-            public void onResponse(@NonNull Call<RecipeSearchResponse> call, @NonNull Response<RecipeSearchResponse> response) {
-                if (response.isSuccessful()) {
-                    RecipeSearchResponse recipeSearchResponse = response.body();
-                    if (recipeSearchResponse != null && recipeSearchResponse.getRecipes() != null) {
-                        List<Recipe> recipes = recipeSearchResponse.getRecipes();
-                        Log.d("MainActivity", "Number of recipes: " + recipes.size());
-                        recipeAdapter.setRecipes(recipes); // Mettre à jour la liste des recettes dans l'adaptateur
-                    }
-                } else {
-
-                    Log.e("RecipeSearch", "Erreur lors de la recherche de recettes : " + response.message());
-                }
+            public void didFetch(RandomRecipeApiResponse response, String message) {
+                dialog.dismiss();
+            recyclerView=findViewById(R.id.recyclerView);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this,1));
+            randomRecipeAdapter=new RandomRecipeAdapter(MainActivity.this,response.recipes);
+            recyclerView.setAdapter(randomRecipeAdapter);
             }
 
             @Override
-            public void onFailure(@NonNull Call<RecipeSearchResponse> call, @NonNull Throwable t) {
-
-                Log.e("RecipeSearch", "Erreur réseau lors de la recherche de recettes : " + t.getMessage());
+            public void didError(String message) {
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
             }
-        });
-    }
+        };
+
+
+
 }
